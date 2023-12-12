41ea512c216aa4634dac00c2ca624e8acb37141d
Krishna Khong <krishna@khong.dev>
2023-12-12 17:20:20 +0800

    feat: add resize grid function

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const GRID_SIZE = 16;
 
 /**
+ * Removes all children of a given node.
+ * @param {Node} parent 
+ */
+function removeChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+/**
  * Creates a grid of a specified number of rows and columns.
  * @param {int} rows 
  * @param {int} cols 
  */
 function initialise_grid(rows, cols) {
     const canvas = document.querySelector('.canvas');
+    removeChildren(canvas);
+
     for (let i = 0; i < rows; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
@@ -35,6 +47,26 @@ function highlight_cells_on_hover() {
     }
 }
 
+function resize_grid() {
+    const resizeButton = document.querySelector('#resize');
+    resizeButton.addEventListener("click", (event) => {
+        let result = prompt("Enter a new grid size: ");
+        if (result === null || result === "") {
+            alert("Please enter a number between 1 and 100.");
+            return;
+        }
+
+        let newGridSize = parseInt(result);
+        if (newGridSize > 1 || newGridSize < 100) {
+            initialise_grid(newGridSize, newGridSize);
+            highlight_cells_on_hover();
+        } else {
+            alert("Please enter a number between 1 and 100.");
+        }
+    });
+}
+
 /** Main */
 initialise_grid(GRID_SIZE, GRID_SIZE);
 highlight_cells_on_hover();
+resize_grid();
